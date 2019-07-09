76b44e0ffc53edece17d1ba082f6878997b6a15e
CalmDownVal <vaclav.lahuta@gmail.com>
2019-07-10 01:51:49 +0200

    MakeContentMap to look more like regular Map

--- src/ContentMap.mjs
+++ src/ContentMap.mjs
@@ -82,6 +82,17 @@ function match(a, b)
 export default class ContentMap
 {
 	#data;
+	#indexOf = mime =>
+	{
+		for (let i = 0; i < this.#data.length; i += 2)
+		{
+			if (equals(this.#data[i], mime))
+			{
+				return i;
+			}
+		}
+		return -1;
+	};
 
 	constructor()
 	{
@@ -98,103 +109,92 @@ export default class ContentMap
 		return new ContentMapIterator(this.#data);
 	}
 
-	add(mime, value)
+	get(mime)
+	{
+		let bestMatch;
+		if ((mime = coerce(mime)))
+		{
+			let bestLevel = 0;
+			for (let i = 0; i < this.#data.length; i += 2)
+			{
+				const level = match(this.#data[i], mime);
+				if (level > bestLevel)
+				{
+					bestMatch = this.#data[i + 1];
+					bestLevel = level;
+
+					if (level === M_FULL)
+					{
+						break;
+					}
+				}
+			}
+		}
+		return bestMatch;
+	}
+
+	getAll(mime)
+	{
+		const list = [];
+		if ((mime = coerce(mime)))
+		{
+			for (let i = 0; i < this.#data.length; i += 2)
+			{
+				switch (match(this.#data[i], mime))
+				{
+					case M_NONE:
+						break;
+
+					case M_FULL:
+						list.unshift(this.#data[i + 1]);
+						break;
+
+					default:
+						list.push(this.#data[i + 1]);
+						break;
+				}
+			}
+		}
+		return list;
+	}
+
+	has(mime)
 	{
 		if ((mime = coerce(mime)))
 		{
-			if (this.indexOf(mime) === -1)
+			for (let i = 0; i < this.#data.length; i += 2)
+			{
+				if (match(this.#data[i], mime))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	set(mime, value)
+	{
+		if ((mime = coerce(mime)))
+		{
+			if (this.#indexOf(mime) === -1)
 			{
 				this.#data.push(mime, value);
 			}
 		}
 	}
 
-	remove(mime)
+	delete(mime)
 	{
-		const i = this.indexOf(coerce(mime));
-		if (i !== -1)
+		if ((mime = coerce(mime)))
 		{
-			this.#data.splice(i, 2);
-			return true;
+			const i = this.indexOf(mime);
+			if (i !== -1)
+			{
+				this.#data.splice(i, 2);
+				return true;
+			}
 		}
-
 		return false;
-	}
-
-	match(mime)
-	{
-		if (!(mime = coerce(mime)))
-		{
-			return null;
-		}
-
-		let bestMatch = null;
-		let bestLevel = 0;
-
-		for (let i = 0; i < this.#data.length; i += 2)
-		{
-			const level = match(this.#data[i], mime);
-			if (level > bestLevel)
-			{
-				bestMatch = this.#data[i + 1];
-				bestLevel = level;
-
-				if (level === M_FULL)
-				{
-					break;
-				}
-			}
-		}
-
-		if (bestLevel === 0)
-		{
-			return null;
-		}
-
-		return bestMatch;
-	}
-
-	matchAll(mime)
-	{
-		const list = [];
-
-		if (!(mime = coerce(mime)))
-		{
-			return list;
-		}
-
-		for (let i = 0; i < this.#data.length; i += 2)
-		{
-			switch (match(this.#data[i], mime))
-			{
-				case M_NONE:
-					break;
-
-				case M_FULL:
-					list.unshift(this.#data[i + 1]);
-					break;
-
-				default:
-					list.push(this.#data[i + 1]);
-					break;
-			}
-		}
-
-		return list;
-	}
-
-	indexOf(mime)
-	{
-		if (mime)
-		{
-			for (let i = 0; i < this.#data.length; i += 2)
-			{
-				if (equals(this.#data[i], mime))
-				{
-					return i;
-				}
-			}
-		}
-		return -1;
 	}
 }
--- test/ContentMap.test.mjs
+++ test/ContentMap.test.mjs
@@ -6,48 +6,52 @@ import { ContentMap } from '../src/index.mjs';
 describe('matching types', () =>
 {
 	const map = new ContentMap();
-	map.add('*/*', 1);
-	map.add('*/*+xml', 2);
-	map.add('text/*', 3);
-	map.add('*/json', 4);
-	map.add('image/png', 5);
-	map.add('text/document+xml', 6);
+	map.set('*/*+xml', 1);
+	map.set('text/*', 2);
+	map.set('*/json', 3);
+	map.set('image/png', 4);
+	map.set('text/document+xml', 5);
 
 	it('should prefer exact match', () =>
 	{
-		strictEqual(map.match('audio/ogg'), 1);
-		strictEqual(map.match('example/document+xml'), 2);
-		strictEqual(map.match('text/whatever'), 3);
-		strictEqual(map.match('application/json'), 4);
-		strictEqual(map.match('image/png'), 5);
+		strictEqual(map.get('example/document+xml'), 1);
+		strictEqual(map.get('text/whatever'), 2);
+		strictEqual(map.get('application/json'), 3);
+		strictEqual(map.get('image/png'), 4);
+	});
+
+	it('should detect presence', () =>
+	{
+		strictEqual(map.has('audio/ogg'), false);
+		strictEqual(map.has('example/document+xml'), true);
 	});
 
 	it('should sort multiple matches by importance', () =>
 	{
 		deepStrictEqual(
-			map.matchAll('text/document+xml'),
-			[ 6, 1, 2, 3 ]);
+			map.getAll('text/document+xml'),
+			[ 5, 1, 2 ]);
 	});
 });
 
 describe('no suffix information', () =>
 {
 	const map = new ContentMap();
-	map.add('*/json', 1);
+	map.set('*/json', 1);
 
 	it('should not match anything', () =>
 	{
 		strictEqual(
-			map.match('text/plain'),
-			null);
+			map.get('text/plain'),
+			undefined);
 	});
 });
 
 describe('iterable protocol', () =>
 {
 	const map = new ContentMap();
-	map.add('*/json', 1);
-	map.add('text/plain', 2);
+	map.set('*/json', 1);
+	map.set('text/plain', 2);
 
 	it('should give the correct size', () =>
 	{

1745de297b8c54140239158376780fe137966113
CalmDownVal <vaclav.lahuta@gmail.com>
2019-11-10 22:18:22 +0100

    crank that coverage

--- test/ContentMap.test.mjs
+++ test/ContentMap.test.mjs
@@ -3,74 +3,103 @@
 import { strictEqual, deepStrictEqual } from 'assert';
 import { ContentMap } from '../src/index.mjs';
 
-describe('matching types', () =>
+describe('ContentMap', () =>
 {
-	const map = new ContentMap();
-	map.set('*/*+xml', 1);
-	map.set('text/*', 2);
-	map.set('*/json', 3);
-	map.set('image/png', 4);
-	map.set('text/document+xml', 5);
-
-	it('should prefer exact match', () =>
+	describe('matching types', () =>
 	{
-		strictEqual(map.get('example/document+xml'), 1);
-		strictEqual(map.get('text/whatever'), 2);
-		strictEqual(map.get('application/json'), 3);
-		strictEqual(map.get('image/png'), 4);
-	});
+		const map = new ContentMap();
+		map.set('*/*+xml', 1);
+		map.set('text/*', 2);
+		map.set('*/json', 3);
+		map.set('image/png', 4);
+		map.set('application/document+xml', 5);
 
-	it('should detect presence', () =>
-	{
-		strictEqual(map.has('audio/ogg'), false);
-		strictEqual(map.has('example/document+xml'), true);
-	});
-
-	it('should sort multiple matches by importance', () =>
-	{
-		deepStrictEqual(
-			map.getAll('text/document+xml'),
-			[ 5, 1, 2 ]);
-	});
-});
-
-describe('no suffix information', () =>
-{
-	const map = new ContentMap();
-	map.set('*/json', 1);
-
-	it('should not match anything', () =>
-	{
-		strictEqual(
-			map.get('text/plain'),
-			undefined);
-	});
-});
-
-describe('iterable protocol', () =>
-{
-	const map = new ContentMap();
-	map.set('*/json', 1);
-	map.set('text/plain', 2);
-
-	it('should give the correct size', () =>
-	{
-		// this is not a part of the iterable protocol, but is very handy nontheless
-		strictEqual(map.size, 2);
-	});
-
-	it('should be iterable', () =>
-	{
-		const values = [];
-		for (const value of map)
+		it('should prefer exact match', () =>
 		{
-			values.push(value.slice(0, 2));
-		}
+			strictEqual(map.get('example/document+xml'), 1);
+			strictEqual(map.get('text/whatever'), 2);
+			strictEqual(map.get('application/json'), 3);
+			strictEqual(map.get('image/png'), 4);
+		});
 
-		deepStrictEqual(values,
-			[
-				[ '*/json', 1 ],
-				[ 'text/plain', 2 ]
-			]);
+		it('should detect presence', () =>
+		{
+			strictEqual(map.has('audio/ogg'), false);
+			strictEqual(map.has('example/document+xml'), true);
+		});
+
+		it('should sort multiple matches by importance', () =>
+		{
+			deepStrictEqual(
+				map.getAll('application/document+xml'),
+				[ 5, 1 ]);
+		});
+
+		it('suffix mismatch', () =>
+		{
+			strictEqual(map.get('application/document+wbxml'), undefined);
+		});
+	});
+
+	describe('no suffix information', () =>
+	{
+		const map = new ContentMap();
+		map.set('*/json', 1);
+
+		it('should not match anything', () =>
+		{
+			strictEqual(
+				map.get('text/plain'),
+				undefined);
+		});
+	});
+
+	describe('iterable protocol', () =>
+	{
+		const map = new ContentMap();
+		map.set('*/json', 1);
+		map.set('text/plain', 2);
+
+		it('should give the correct size', () =>
+		{
+			// this is not a part of the iterable protocol, but is very handy nontheless
+			strictEqual(map.size, 2);
+		});
+
+		it('should be iterable', () =>
+		{
+			const values = [];
+			for (const value of map)
+			{
+				values.push(value.slice(0, 2));
+			}
+
+			deepStrictEqual(values,
+				[
+					[ '*/json', 1 ],
+					[ 'text/plain', 2 ]
+				]);
+		});
+	});
+
+	describe('setting & deleting items', () =>
+	{
+		const map = new ContentMap();
+
+		it('should ignore invalid inputs', () =>
+		{
+			map.set(123, 456);
+			strictEqual(map.delete(123), false);
+			strictEqual(map.size, 0);
+		});
+
+		it('should not delete other patterns', () =>
+		{
+			map.set('text/plain', 1);
+			map.set('text/*', 2);
+			map.delete('text/*');
+			strictEqual(map.size, 1);
+			strictEqual(map.get('text/*'), 1);
+		});
 	});
 });
--- test/parse.test.mjs
+++ test/parse.test.mjs
@@ -3,139 +3,142 @@
 import { ok, strictEqual, deepStrictEqual } from 'assert';
 import { parse } from '../src/index.mjs';
 
-describe('top-level types', () =>
+describe('parse', () =>
 {
-	it('should match registered top-level types', () =>
+	describe('top-level types', () =>
 	{
-		const result = parse('example/sub-type');
-		strictEqual(
-			result && result.type.name,
-			'example');
+		it('should match registered top-level types', () =>
+		{
+			const result = parse('example/sub-type');
+			strictEqual(
+				result && result.type.name,
+				'example');
+		});
+
+		it('should match extension types', () =>
+		{
+			const result = parse('x-some-type/sub-type');
+			strictEqual(
+				result && result.type.isExtension,
+				true);
+		});
+
+		it('should not match invalid types', () =>
+		{
+			strictEqual(parse('invalid-type/sub-type'), null);
+		});
 	});
 
-	it('should match extension types', () =>
+	describe('sub-types', () =>
 	{
-		const result = parse('x-some-type/sub-type');
-		strictEqual(
-			result && result.type.isExtension,
-			true);
+		it('should parse out sub-type', () =>
+		{
+			const result = parse('example/sub-type');
+			strictEqual(
+				result && result.subType.name,
+				'sub-type');
+		});
+
+		it('should recognize valid tree prefixes', () =>
+		{
+			const result1 = parse('example/vnd.sub-type');
+			strictEqual(
+				result1 && result1.subType.tree,
+				'vnd');
+
+			const result2 = parse('example/prs.sub-type');
+			strictEqual(
+				result2 && result2.subType.tree,
+				'prs');
+
+			const result3 = parse('example/xxx.sub-type');
+			strictEqual(
+				result3 && result3.subType.tree,
+				null);
+		});
+
+		it('should recognize suffixes', () =>
+		{
+			const result1 = parse('example/sub-type+xml');
+			strictEqual(result1 && result1.subType.suffix,
+				'xml');
+
+			const result2 = parse('example/sub-type');
+			strictEqual(result2 && result2.subType.suffix,
+				null);
+
+			strictEqual(parse('example/sub-type+bad'), null);
+		});
 	});
 
-	it('should not match invalid types', () =>
+	describe('parameters', () =>
 	{
-		strictEqual(parse('invalid-type/sub-type'), null);
-	});
-});
+		it('should parse out parameters (lowercased)', () =>
+		{
+			const result = parse('example/sub-type; ChArSeT=utf-8; bad');
+			deepStrictEqual(
+				result && result.parameters,
+				{ charset : 'utf-8' });
+		});
 
-describe('sub-types', () =>
-{
-	it('should parse out sub-type', () =>
-	{
-		const result = parse('example/sub-type');
-		strictEqual(
-			result && result.subType.name,
-			'sub-type');
-	});
-
-	it('should recognize valid tree prefixes', () =>
-	{
-		const result1 = parse('example/vnd.sub-type');
-		strictEqual(
-			result1 && result1.subType.tree,
-			'vnd');
-
-		const result2 = parse('example/prs.sub-type');
-		strictEqual(
-			result2 && result2.subType.tree,
-			'prs');
-
-		const result3 = parse('example/xxx.sub-type');
-		strictEqual(
-			result3 && result3.subType.tree,
-			null);
+		it('should handle comments', () =>
+		{
+			ok(parse('example/sub-type; ChArSeT=utf-8 (Im a comment!)'));
+		});
 	});
 
-	it('should recognize suffixes', () =>
+	describe('pattern wildcards', () =>
 	{
-		const result1 = parse('example/sub-type+xml');
-		strictEqual(result1 && result1.subType.suffix,
-			'xml');
+		it('should reject wildcards by default', () =>
+		{
+			strictEqual(parse('*/javascript'), null);
+		});
 
-		const result2 = parse('example/sub-type');
-		strictEqual(result2 && result2.subType.suffix,
-			null);
-
-		strictEqual(parse('example/sub-type+bad'), null);
-	});
-});
-
-describe('parameters', () =>
-{
-	it('should parse out parameters (lowercased)', () =>
-	{
-		const result = parse('example/sub-type; ChArSeT=utf-8');
-		deepStrictEqual(
-			result && result.parameters,
-			{ charset : 'utf-8' });
-	});
-
-	it('should handle comments', () =>
-	{
-		ok(parse('example/sub-type; ChArSeT=utf-8 (Im a comment!)'));
-	});
-});
-
-describe('pattern wildcards', () =>
-{
-	it('should reject wildcards by default', () =>
-	{
-		strictEqual(parse('*/javascript'), null);
-	});
-
-	it('should parse wildcards when explicitly enabled', () =>
-	{
-		deepStrictEqual(
-			parse('*/*+*', true),
-			{
-				type :
+		it('should parse wildcards when explicitly enabled', () =>
+		{
+			deepStrictEqual(
+				parse('*/*+*', true),
 				{
-					name : '*',
-					isExtension : false
-				},
-				subType :
-				{
-					name : '*+*',
-					tree : null,
-					suffix : '*'
-				},
-				parameters : {}
-			});
+					type :
+					{
+						name : '*',
+						isExtension : false
+					},
+					subType :
+					{
+						name : '*+*',
+						tree : null,
+						suffix : '*'
+					},
+					parameters : {}
+				});
+		});
 	});
-});
 
-describe('final test', () =>
-{
-	it('should parse a complex type', () =>
+	describe('final test', () =>
 	{
-		deepStrictEqual(
-			parse('application/vnd.openstreetmap.data+xml; charset=utf-8; NeEdS="To (be) quoteD" (hello world)'),
-			{
-				type :
+		it('should parse a complex type', () =>
+		{
+			deepStrictEqual(
+				parse('application/vnd.openstreetmap.data+xml; charset=utf-8; NeEdS="To (be) quoteD" (hello world)'),
 				{
-					name : 'application',
-					isExtension : false
-				},
-				subType :
-				{
-					name : 'vnd.openstreetmap.data+xml',
-					tree : 'vnd',
-					suffix : 'xml'
-				},
-				parameters :
-				{
-					charset : 'utf-8',
-					needs : 'To (be) quoteD'
-				}
-			});
+					type :
+					{
+						name : 'application',
+						isExtension : false
+					},
+					subType :
+					{
+						name : 'vnd.openstreetmap.data+xml',
+						tree : 'vnd',
+						suffix : 'xml'
+					},
+					parameters :
+					{
+						charset : 'utf-8',
+						needs : 'To (be) quoteD'
+					}
+				});
+		});
 	});
 });
